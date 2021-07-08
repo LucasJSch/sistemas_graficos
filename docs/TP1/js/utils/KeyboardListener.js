2220a3749cb85ee6a1ae00c517c0b628399ec8fb
LucasJSch <lscheink@creativa77.com.ar>
2021-07-08 14:01:10 -0300

    Slight modification to KeyboardListener.

--- docs/TP1/js/utils/KeyboardListener.js
+++ docs/TP1/js/utils/KeyboardListener.js
@@ -1,32 +1,35 @@
 class KeyboardListener {
     // Registers listeners to key events.
-    // Each listener must have a execute() method.
+    // Each listener must have a execute(e) method.
     constructor() {
-        this.keydown_listeners = {};
-        this.keyup_listeners = {};
+        this.keydown_listeners = [];
+        this.keyup_listeners = [];
     }
 
-    registerKeyDownListener(key, listener) {
-        this.keydown_listeners[key] = listener;
-        console.log(this.keydown_listeners);
+    registerKeyDownListener(listener) {
+        this.keydown_listeners.push(listener);
     }
     
-    registerKeyUpListener(key, listener) {
-        this.keyup_listeners[key] = listener;
+    registerKeyUpListener(listener) {
+        this.keyup_listeners.push(listener);
     }
 
     initialize() {
         var keydown_listeners = this.keydown_listeners;
         document.addEventListener("keydown", function(e) {
-            if (keydown_listeners.hasOwnProperty(e.key)) {
-                keydown_listeners[e.key].execute();
+            if (keydown_listeners != null) {
+                for (var i = 0; i < keydown_listeners.length; i++) {
+                    keydown_listeners[i].execute(e);
+                }
             }
         });
 
         var keyup_listeners = this.keyup_listeners;
         document.addEventListener("keyup", function(e) {
-            if (keyup_listeners.hasOwnProperty(e.key)) {
-                keyup_listeners[e.key].execute();
+            if (keyup_listeners != null) {
+                for (var i = 0; i < keyup_listeners.length; i++) {
+                    keyup_listeners[i].execute(e);
+                }
             }
         });
     }
