102693cb8370d2e491adf3a5b2765f041a607905
LucasJSch <lscheink@creativa77.com.ar>
2021-07-09 01:53:18 -0300

    Crane movements complete except for F rotation.

--- docs/TP1/js/models/Crane/CraneLoad.js
+++ docs/TP1/js/models/Crane/CraneLoad.js
@@ -22,6 +22,7 @@ class CraneLoad {
             transformMatrix = mat4.create();
         }
 
+        console.log(this.length_offset);
         this.createTransformationMatrices(transformMatrix);
         this.mainHolder.draw(this.mainHolder_transf);
         this.holder1.draw(this.holder1_transf);
@@ -48,14 +49,17 @@ class CraneLoad {
         
         // Scaling matrices:
         var mainHolder_s = mat4.create();
-        this.mainHolder_transf = mainHolder_s;
         mat4.fromScaling(mainHolder_s,  [0.01, 0.01, 3.0 + this.length_offset]);
         var secondary_holders_s = mat4.create();
         mat4.fromScaling(secondary_holders_s,  [0.01, 0.01, 2.0]);
-
+        
         // Translation matrices:
+        var mainHolder_t = mat4.create();
+        mat4.fromTranslation(mainHolder_t, [0.0, 0.0, -this.length_offset]);
+        mat4.mul(this.mainHolder_transf, mainHolder_t, mainHolder_s);
         var secondary_holders_t = mat4.create();
-        mat4.fromTranslation(secondary_holders_t, [0.0, 0.0, -1.0 - + this.length_offset]);
+        mat4.fromTranslation(secondary_holders_t, [0.0, 0.0, -1.0]);
+        // mat4.mul(secondary_holders_t, secondary_holders_t, mainHolder_t);
         
         // Secondary holders are distinguished from one another by their rotations.
         // They have the same translation and scaling.
@@ -74,13 +78,18 @@ class CraneLoad {
         mat4.mul(this.holder3_transf, aux, this.holder2_transf);
         mat4.mul(this.holder4_transf, aux, this.holder3_transf);
 
+        mat4.mul(this.holder1_transf, mainHolder_t, this.holder1_transf);
+        mat4.mul(this.holder2_transf, mainHolder_t, this.holder2_transf);
+        mat4.mul(this.holder3_transf, mainHolder_t, this.holder3_transf);
+        mat4.mul(this.holder4_transf, mainHolder_t, this.holder4_transf);
+
         mat4.fromScaling(aux, [2.8, 2.8, 0.1]);
-        mat4.fromTranslation(this.base_transf, [0.0, 0.0, -0.90]);
+        mat4.fromTranslation(this.base_transf, [0.0, 0.0, -0.90 - this.length_offset]);
         mat4.mul(this.base_transf, this.base_transf, aux);
         mat4.fromRotation(aux, 0.1, [0.0, 0.0, 1.0], [1.0, 0.0, 0.0]);
         mat4.mul(this.base_transf, aux, this.base_transf);
 
-        // Apply transformMatrix/
+        // Apply transformMatrix.
         mat4.mul(this.base_transf, transformMatrix, this.base_transf);
         mat4.mul(this.holder1_transf, transformMatrix, this.holder1_transf);
         mat4.mul(this.holder2_transf, transformMatrix, this.holder2_transf);
