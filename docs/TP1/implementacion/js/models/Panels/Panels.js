e3d8303e941222967dae0a61cad03f5ec6709dd3
LucasJSch <lscheink@creativa77.com.ar>
2021-10-30 14:54:13 -0300

    [2C2021] Paneles casi terminados.

--- docs/TP1/implementacion/js/models/Panels/Panels.js
+++ docs/TP1/implementacion/js/models/Panels/Panels.js
@@ -1,9 +1,12 @@
 class Panels {
     // Draws the solar panels.
+    // Must have at least 4 panels per side.
     constructor(glProgram) {
         this.glProgram = glProgram;
         this.cylinder_color = [0.794117647, 0.7, 0.809803922];
-        this.n_panels_per_side = 4.0; 
+        this.n_panels_per_side = 4.0;
+        this.panel_dist = 1.5;
+        this.cylinder_length = 8.0 + (this.n_panels_per_side - 4.0) * this.panel_dist;
         this.cylinder = new Cylinder(glProgram, this.cylinder_color);
 
         this.utils = new Utils();
@@ -14,11 +17,35 @@ class Panels {
             transformMatrix = mat4.create();
         }
 
-        // var t_cylinder = mat4.create();
-        // mat4.fromScaling(t_cylinder, [0.2, 0.2, 5.0]);
-        // mat4.mul(t_cylinder, transformMatrix, t_cylinder);
-        // this.cylinder.draw(t_cylinder);
+        var t_cylinder = mat4.create();
+        mat4.fromScaling(t_cylinder, [0.15, 0.15,  this.cylinder_length]);
+        mat4.mul(t_cylinder, transformMatrix, t_cylinder);
+        this.cylinder.draw(t_cylinder);
+
+        for (var i = 0; i < this.n_panels_per_side; i++) {
+            this.drawSolarPanel(transformMatrix, i, false);
+            this.drawSolarPanel(transformMatrix, i, true);
+        }
+    }
+
+    // number: int that says which number of panel it is
+    // left: boolean that decides to draw on left or right
+    drawSolarPanel(transformMatrix, number, left) {
+        const init_dist = 0.2;
+
+        var aux = mat4.create();
+        var t_solar_panel = mat4.create();
+
+        if (left) {
+            mat4.fromRotation(t_solar_panel, -Math.PI/2.0, [0.0, 1.0, 0.0]);
+        } else {
+            mat4.fromRotation(t_solar_panel, +Math.PI/2.0, [0.0, 1.0, 0.0]);
+        }
+        
+        mat4.fromTranslation(aux, [0.0, 0.0, init_dist + this.panel_dist * number]);
+        mat4.mul(t_solar_panel, aux, t_solar_panel);
+        mat4.mul(t_solar_panel, transformMatrix, t_solar_panel);
         var solarPanel = new SolarPanel(this.glProgram);
-        solarPanel.draw(transformMatrix);
+        solarPanel.draw(t_solar_panel);
     }
 }
--- docs/TP1/implementacion/js/models/Panels/SolarPanel.js
+++ docs/TP1/implementacion/js/models/Panels/SolarPanel.js
@@ -3,7 +3,7 @@ class SolarPanel {
     constructor(glProgram) {
         this.glProgram = glProgram;
         this.plane_color = [0.823529412, 0.662745098, 0.53333333];
-        this.cylinder_color = [0.794117647, 0.7, 0.809803922];
+        this.cylinder_color = [0.873529412, 0.712745098, 0.58333333];
         this.cylinder = new Cylinder(glProgram, this.cylinder_color);
         this.plane = new Cube(glProgram, this.plane_color);
 
@@ -23,7 +23,7 @@ class SolarPanel {
         var aux = mat4.create();
         var t_plane = mat4.create();
 
-        mat4.fromScaling(t_plane, [1.0, 4.0, 0.1]);
+        mat4.fromScaling(t_plane, [1.0, 4.0, 0.2]);
         mat4.fromRotation(aux, -Math.PI/2.0, [1.0, 0.0, 0.0]);
         mat4.mul(t_plane, aux, t_plane);
 
