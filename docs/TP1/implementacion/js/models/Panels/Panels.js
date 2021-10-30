93fc4c8aeff91ecb430ab98898db921d336b6a32
LucasJSch <lscheink@creativa77.com.ar>
2021-10-30 15:00:51 -0300

    [2C2021] Paneles terminados.

--- docs/TP1/implementacion/js/models/Panels/Panels.js
+++ docs/TP1/implementacion/js/models/Panels/Panels.js
@@ -8,6 +8,7 @@ class Panels {
         this.panel_dist = 1.5;
         this.cylinder_length = 8.0 + (this.n_panels_per_side - 4.0) * this.panel_dist;
         this.cylinder = new Cylinder(glProgram, this.cylinder_color);
+        this.panel_rotation_rad = Math.PI / 4.0;
 
         this.utils = new Utils();
     }
@@ -36,10 +37,13 @@ class Panels {
         var aux = mat4.create();
         var t_solar_panel = mat4.create();
 
+        mat4.fromRotation(aux, this.panel_rotation_rad, [1.0, 0.0, 0.0]);
         if (left) {
             mat4.fromRotation(t_solar_panel, -Math.PI/2.0, [0.0, 1.0, 0.0]);
+            mat4.mul(t_solar_panel, aux, t_solar_panel);
         } else {
             mat4.fromRotation(t_solar_panel, +Math.PI/2.0, [0.0, 1.0, 0.0]);
+            mat4.mul(t_solar_panel, aux, t_solar_panel);
         }
         
         mat4.fromTranslation(aux, [0.0, 0.0, init_dist + this.panel_dist * number]);
