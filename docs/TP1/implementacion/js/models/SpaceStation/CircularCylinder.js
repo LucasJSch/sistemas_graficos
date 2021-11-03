066bdd5a72a8b90e27534f6119b6550dfbc34b6e
LucasJSch <lscheink@creativa77.com.ar>
2021-11-03 20:54:17 -0300

    [2C2021] Finished CircularCylinder.

--- docs/TP1/implementacion/js/models/SpaceStation/CircularCylinder.js
+++ docs/TP1/implementacion/js/models/SpaceStation/CircularCylinder.js
@@ -2,15 +2,13 @@ class CircularCylinder {
     constructor(glProgram) {
         this.glProgram = glProgram;
         this.color = [1.0, 0.0, 0.0];
-        this.points_per_shape = 10.0;
-        this.radius = 2.0;
-        // this.bezier_points = [[0.0, 0.0, 0.0], [1.0, 0.0, 0.0], [1.0, 2.0, 0.0], [0.0, 2.0, 0.0],
-        //                       [0.0, 2.0, 0.0], [-1.0, 2.0, 0.0], [-1.0, 0.0, 0.0], [0.0, 0.0, 0.0]];
         this.bezier_concatenator = null;
         // The curve in this case is the shape to draw multiple times.
-        this.n_curves = 10.0;
-        this.n_points_per_curve = 10.0;
-        // this.n_bezier_pts = this.bezier_points.length / 4.0;
+        // Use odd numbers here.
+        this.n_curves = 51.0;
+        this.radius = 0.3;
+        this.n_points_per_curve = 50.0;
+        this.distance_from_zero = 5.0;
 
         this.pos_buf = [];
         this.nrm_buf = [];
@@ -36,7 +34,7 @@ class CircularCylinder {
         }
 
 
-        var grid = new Grid(this.glProgram, this.pos_buf, this.nrm_buf, this.clr_buf, /*n_rows=*/this.n_points_per_curve, /*n_cols=*/this.n_curves);
+        var grid = new Grid(this.glProgram, this.pos_buf, this.nrm_buf, this.clr_buf, /*n_rows=*/this.n_curves + 1.0, /*n_cols=*/this.n_points_per_curve + 1.0);
         grid.draw(transformMatrix);
     }
 
@@ -44,15 +42,15 @@ class CircularCylinder {
         // TODO: Compute once and then transform.
         if (this.base_shape_pos == null) {
             this.base_shape_pos = [];
-            for (var i = 0.0; i < this.points_per_shape; i++) {
-                this.base_shape_pos.push(1.0 + this.radius*Math.cos(i * 2.0 * Math.PI / this.points_per_shape));
-                this.base_shape_pos.push(this.radius*Math.sin(i * 2.0 * Math.PI / this.points_per_shape));
+            for (var i = 0.0; i < this.n_points_per_curve; i++) {
+                this.base_shape_pos.push(this.distance_from_zero + this.radius * Math.cos(i * 2.0 * Math.PI / this.n_points_per_curve));
+                this.base_shape_pos.push(this.radius * Math.sin(i * 2.0 * Math.PI / this.n_points_per_curve));
                 this.base_shape_pos.push(0.0);
             }
             // We need to connect the shape's end and beginning.
-            // Because of this we iterate in the other functions until points_per_shape + 1.
-            this.base_shape_pos.push(1.0 + Math.cos(0.0));
-            this.base_shape_pos.push(Math.sin(0.0));
+            // Because of this we iterate in the other functions until n_points_per_curve + 1.
+            this.base_shape_pos.push(this.distance_from_zero + this.radius * Math.cos(0.0));
+            this.base_shape_pos.push(this.radius * Math.sin(0.0));
             this.base_shape_pos.push(0.0);
         }
 
