b3b663671587b01568ed4b63240d5916ac56af55
LucasJSch <lscheink@creativa77.com.ar>
2021-11-03 20:45:36 -0300

    [2C2021] Implementacion parcial CircularCylinder.

--- docs/TP1/implementacion/js/drawScene.js
+++ docs/TP1/implementacion/js/drawScene.js
@@ -14,6 +14,6 @@ function drawScene(glProgram, modelMatrix, viewMatrix, projMatrix, normalMatrix)
     var coordinates = new Coordinates(glProgram);
     coordinates.draw();
 
-    var planet = new Nucleus(glProgram);
+    var planet = new CircularCylinder(glProgram);
     planet.draw();
 }
