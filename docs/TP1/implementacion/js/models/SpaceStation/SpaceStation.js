e146b9f9dc4b10073d456408c8f87f7ecf2cbd7b
LucasJSch <lscheink@creativa77.com.ar>
2021-11-06 12:16:59 -0300

    [2C2021] Agrego paneles a estacion espacial.

--- docs/TP1/implementacion/js/models/SpaceStation/SpaceStation.js
+++ docs/TP1/implementacion/js/models/SpaceStation/SpaceStation.js
@@ -7,6 +7,7 @@ class SpaceStation {
         this.columns = new CraneColumns(glProgram, this.n_sections);
         this.cylinder = new CircularCylinder(glProgram);
         this.nucleus = new Nucleus(glProgram);
+        this.panels = new Panels(glProgram);
     }
 
     draw(transformMatrix) {
@@ -14,16 +15,24 @@ class SpaceStation {
             transformMatrix = mat4.create();
         }
 
-        
-        var t = mat4.create();
         var aux = mat4.create();
+
+        var t_panels = mat4.create();
+        mat4.fromRotation(t_panels, Math.PI / 2.0, [1.0, 0.0, 0.0]);
+        mat4.fromTranslation(aux, [0.0, 17.0, 0.0]);
+        mat4.mul(t_panels, aux, t_panels);
+        mat4.mul(t_panels, transformMatrix, t_panels);
+
+        var t = mat4.create();
         mat4.fromRotation(t, Math.PI / 2.0, [0.0, 0.0, 1.0]);
         mat4.fromTranslation(aux, [0.0, -7.0, 0.0]);
         mat4.mul(t, aux, t);
         mat4.mul(t, transformMatrix, t);
         this.nucleus.draw(t);
+
         this.rectangles.draw(transformMatrix);
         this.columns.draw(transformMatrix);
         this.cylinder.draw(transformMatrix);
+        this.panels.draw(t_panels);
     }
 }
