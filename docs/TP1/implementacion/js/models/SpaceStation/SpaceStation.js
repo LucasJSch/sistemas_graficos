a6d1c356d6ec6dbc85bdd9155838a15447490025
LucasJSch <lscheink@creativa77.com.ar>
2021-11-06 14:54:56 -0300

    [2C2021] Agrego cilindro del centro de estacion espacial.

--- docs/TP1/implementacion/js/models/SpaceStation/SpaceStation.js
+++ docs/TP1/implementacion/js/models/SpaceStation/SpaceStation.js
@@ -8,6 +8,7 @@ class SpaceStation {
         this.cylinder = new CircularCylinder(glProgram);
         this.nucleus = new Nucleus(glProgram);
         this.panels = new Panels(glProgram);
+        this.center_cylinder = new Cylinder(glProgram, [0.0, 0.82, 0.12])
     }
 
     draw(transformMatrix) {
@@ -30,9 +31,18 @@ class SpaceStation {
         mat4.mul(t, transformMatrix, t);
         this.nucleus.draw(t);
 
+        var center_cyl_t = mat4.create();
+        mat4.fromRotation(aux, Math.PI / 2.0, [1.0, 0.0, 0.0]);
+        mat4.fromScaling(center_cyl_t, [2.0, 2.0, 0.5]);
+        mat4.mul(center_cyl_t, aux, center_cyl_t);
+        mat4.fromTranslation(aux, [0.0, 0.25, 0.0]);
+        mat4.mul(center_cyl_t, aux, center_cyl_t);
+        mat4.mul(center_cyl_t, transformMatrix, center_cyl_t);
+
         this.rectangles.draw(transformMatrix);
         this.columns.draw(transformMatrix);
         this.cylinder.draw(transformMatrix);
         this.panels.draw(t_panels);
+        this.center_cylinder.draw(center_cyl_t);
     }
 }
