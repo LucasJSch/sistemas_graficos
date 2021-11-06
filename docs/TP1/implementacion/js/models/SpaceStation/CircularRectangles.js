526a7ecd72908a4dfb10423a966f5245015300b2
LucasJSch <lscheink@creativa77.com.ar>
2021-11-06 11:17:23 -0300

    [2C2021] Cambio de seccion cilindrica a rectangular en estacion espacial.

--- docs/TP1/implementacion/js/models/SpaceStation/CircularRectangles.js
+++ docs/TP1/implementacion/js/models/SpaceStation/CircularRectangles.js
@@ -4,9 +4,7 @@ class CircularRectangles {
         this.color = [1.0, 0.0, 0.0];
         // The curve in this case is the shape to draw multiple times.
         this.n_curves_per_section = 12.0;
-        this.radius = 0.6;
-        this.n_points_per_curve = 11.0;
-        this.distance_from_zero = 5.0;
+        this.n_points_per_curve = 8.0;
 
         // Integer bigger than 3.
         this.n_sections = n_rectangles;
@@ -25,7 +23,6 @@ class CircularRectangles {
         }
 
         for (var section = 1.0; section <= this.n_sections; section++) {
-            // var section = 1.0;
             var init_angle = (section - 1.0) * this.angular_length_per_section * 2.0;
             // Correccion para que gire igual que las columnas de la estacion espacial.
             init_angle -= Math.PI / 2.0 + this.angular_length_per_section / 2.0;
@@ -51,15 +48,40 @@ class CircularRectangles {
     getShapePosBuf(angleToRotate) {
         if (this.base_shape_pos == null) {
             this.base_shape_pos = [];
-            for (var i = 0.0; i < this.n_points_per_curve; i++) {
-                this.base_shape_pos.push(this.distance_from_zero + this.radius * Math.cos(i * 2.0 * Math.PI / this.n_points_per_curve));
-                this.base_shape_pos.push(this.radius * Math.sin(i * 2.0 * Math.PI / this.n_points_per_curve));
-                this.base_shape_pos.push(0.0);
-            }
-            // We need to connect the shape's end and beginning.
-            // Because of this we iterate in the other functions until n_points_per_curve + 1.
-            this.base_shape_pos.push(this.distance_from_zero + this.radius * Math.cos(0.0));
-            this.base_shape_pos.push(this.radius * Math.sin(0.0));
+            this.base_shape_pos.push(+4.7);
+            this.base_shape_pos.push(-1.0);
+            this.base_shape_pos.push(0.0);
+
+            this.base_shape_pos.push(+4.5);
+            this.base_shape_pos.push(-0.6);
+            this.base_shape_pos.push(0.0);
+
+            this.base_shape_pos.push(+4.5);
+            this.base_shape_pos.push(+0.6);
+            this.base_shape_pos.push(0.0);
+
+            this.base_shape_pos.push(+4.7);
+            this.base_shape_pos.push(+1.0);
+            this.base_shape_pos.push(0.0);
+
+            this.base_shape_pos.push(5.0);
+            this.base_shape_pos.push(1.0);
+            this.base_shape_pos.push(0.0);
+
+            this.base_shape_pos.push(5.5);
+            this.base_shape_pos.push(0.6);
+            this.base_shape_pos.push(0.0);
+
+            this.base_shape_pos.push(+5.5);
+            this.base_shape_pos.push(-0.6);
+            this.base_shape_pos.push(0.0);
+
+            this.base_shape_pos.push(+5.5);
+            this.base_shape_pos.push(-1.0);
+            this.base_shape_pos.push(0.0);
+
+            this.base_shape_pos.push(+4.7);
+            this.base_shape_pos.push(-1.0);
             this.base_shape_pos.push(0.0);
         }
 
