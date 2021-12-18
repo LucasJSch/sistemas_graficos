0a86a5c32c50652d9e7013e1f0e60a9b7382c0b8
LucasJSch <lscheink@creativa77.com.ar>
2021-12-18 20:22:37 -0300

    Corrijo tapa de NucleusCylinder.

--- docs/TP2/implementacion/js/models/SpaceStation/Nucleus/NucleusCylinder.js
+++ docs/TP2/implementacion/js/models/SpaceStation/Nucleus/NucleusCylinder.js
@@ -1,9 +1,8 @@
 class NucleusCylinder {
     // Draws a cylinder with Bezier-defined surfaces
-    constructor(shader, bezier_points, length) {
+    constructor(shader, bezier_points) {
         this.shader = shader;
-        this.color = [0.823529412, 0.662745098, 0.53333333];
-        this.cylinder_length = length;
+        this.color = [0.0, 0.0, 0.0];
         this.bezier_points = bezier_points;
         this.bezier_concatenator = null;
 
@@ -25,6 +24,7 @@ class NucleusCylinder {
         this.top_uv_buf = [];
 
         this.ptos_base = [];
+        this.nrms_base = [];
         this.utils = new Utils();
 
         this.scale_factor_u = 1.0;
@@ -49,7 +49,6 @@ class NucleusCylinder {
         this.generateBasePts();
         this.generateSideBuffers();
         this.generateTopBottomBuffers();
-        this.applyTransformMatrix(transformMatrix);
 
         var sides_grid = new Grid(this.shader, this.sides_pos_buf, this.sides_nrm_buf, this.color, /*n_rows=*/this.n_rows + 1.0, /*n_cols=*/this.ptos_longitudinal, this.sides_uv_buf);
         sides_grid.setTexture(this.texture);
@@ -57,16 +56,9 @@ class NucleusCylinder {
         top_grid.setTexture(this.texture);
         var bottom_grid = new Grid(this.shader, this.bottom_pos_buf, this.bottom_nrm_buf, this.color, /*n_rows=*/2.0, /*n_cols=*/((this.n_rows + 1.0) / 2.0), this.bottom_uv_buf);
         bottom_grid.setTexture(this.texture);
-        sides_grid.draw();
-        top_grid.draw();
-        bottom_grid.draw();
-    }
-
-    applyTransformMatrix(transformMatrix) {
-        // TODO: Transform normal buf.
-        this.sides_pos_buf = this.utils.TransformPosBuffer(transformMatrix, this.sides_pos_buf);
-        this.top_pos_buf = this.utils.TransformPosBuffer(transformMatrix, this.top_pos_buf);
-        this.bottom_pos_buf = this.utils.TransformPosBuffer(transformMatrix, this.bottom_pos_buf);
+        sides_grid.draw(transformMatrix);
+        top_grid.draw(transformMatrix);
+        bottom_grid.draw(transformMatrix);
     }
 
     generateBezierConcatenator() {
@@ -83,6 +75,10 @@ class NucleusCylinder {
             this.ptos_base.push(aux[0]);
             this.ptos_base.push(aux[1]);
             this.ptos_base.push(aux[2]);
+            aux = this.bezier_concatenator.getSecondDerivative(this.n_points_bezier * i / this.ptos_longitudinal);
+            this.nrms_base.push(aux[0]);
+            this.nrms_base.push(aux[1]);
+            this.nrms_base.push(aux[2]);
         }
     }
 
@@ -98,20 +94,7 @@ class NucleusCylinder {
                 this.sides_uv_buf.push(this.scale_factor_u * 4.0 * i / ptos_rotados.length);
                 this.sides_uv_buf.push(this.scale_factor_v * 1.0 * rotacion / Math.PI);
             }
-        }
-
-        for (var i = 0; i < this.sides_pos_buf.length; i += 3) {
-            // Genero normal cada 3 puntos (i.e. 1 por triangulo)
-            if (i < 6) {
-                continue;
-            }
-            var nrm = this.utils.GetTriangNormal(
-                [this.sides_pos_buf[i-6], this.sides_pos_buf[i-5], this.sides_pos_buf[i-4]],
-                [this.sides_pos_buf[i-3], this.sides_pos_buf[i-2], this.sides_pos_buf[i-1]],
-                [this.sides_pos_buf[i], this.sides_pos_buf[i+1], this.sides_pos_buf[i+2]]);
-            this.sides_nrm_buf.push(nrm[0]);
-            this.sides_nrm_buf.push(nrm[1]);
-            this.sides_nrm_buf.push(nrm[2]);
+            this.sides_nrm_buf = this.sides_nrm_buf.concat(this.nrms_base);
         }
     }
 
@@ -128,30 +111,35 @@ class NucleusCylinder {
             this.bottom_uv_buf.push(0.1);
             this.top_uv_buf.push(0.1);
             this.top_uv_buf.push(0.1);
+            this.bottom_nrm_buf.push(-1.0);
+            this.bottom_nrm_buf.push(0.0);
+            this.bottom_nrm_buf.push(0.0);
+            this.top_nrm_buf.push(1.0);
+            this.top_nrm_buf.push(0.0);
+            this.top_nrm_buf.push(0.0);
         }
         this.bottom_pos_buf.push(this.bottom_pos_buf[0]);
         this.bottom_pos_buf.push(this.bottom_pos_buf[1]);
         this.bottom_pos_buf.push(this.bottom_pos_buf[2]);
+        this.bottom_nrm_buf.push(-1.0);
+        this.bottom_nrm_buf.push(0.0);
+        this.bottom_nrm_buf.push(0.0);
+        this.top_nrm_buf.push(1.0);
+        this.top_nrm_buf.push(0.0);
+        this.top_nrm_buf.push(0.0);
 
-        var t_traslacion = mat4.create();
-        mat4.fromTranslation(t_traslacion, [this.cylinder_length, 0.0, 0.0]);
-        this.top_pos_buf = this.utils.TransformPosBuffer(t_traslacion, this.bottom_pos_buf);
-
-        for (var i = 0; i < this.bottom_pos_buf.length; i += 3) {
-            // Genero normal cada 3 puntos (i.e. 1 por triangulo)
-            if (i < 6) {
-                continue;
-            }
-            var nrm = this.utils.GetTriangNormal(
-                [this.bottom_pos_buf[i-6], this.bottom_pos_buf[i-5], this.bottom_pos_buf[i-4]],
-                [this.bottom_pos_buf[i-3], this.bottom_pos_buf[i-2], this.bottom_pos_buf[i-1]],
-                [this.bottom_pos_buf[i], this.bottom_pos_buf[i+1], this.bottom_pos_buf[i+2]]);
-            this.bottom_nrm_buf.push(nrm[0]);
-            this.bottom_nrm_buf.push(nrm[1]);
-            this.bottom_nrm_buf.push(nrm[2]);
-            this.top_nrm_buf.push(-nrm[0]);
-            this.top_nrm_buf.push(-nrm[1]);
-            this.top_nrm_buf.push(-nrm[2]);
+        for (var i = this.ptos_longitudinal*3-3; i < this.sides_pos_buf.length; i+= this.ptos_longitudinal*3.0) {
+            this.top_pos_buf.push(this.sides_pos_buf[i]);
+            this.top_pos_buf.push(this.sides_pos_buf[i+1]);
+            this.top_pos_buf.push(this.sides_pos_buf[i+2]);
         }
+        this.top_pos_buf.push(this.sides_pos_buf[this.ptos_longitudinal-1]);
+        this.top_pos_buf.push(this.sides_pos_buf[this.ptos_longitudinal]);
+        this.top_pos_buf.push(this.sides_pos_buf[this.ptos_longitudinal+1]);
+
+        
+        console.log(this.top_pos_buf.length);
+        console.log(this.sides_pos_buf.length);
+        console.log(this.ptos_base.length);
     }
 }
