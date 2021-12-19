8786b885b712cabd6339d76eb4656f1e75407cc2
LucasJSch <lscheink@creativa77.com.ar>
2021-12-18 21:12:10 -0300

    Agrego UV para space station cilindro circular.

--- docs/TP2/implementacion/js/models/SpaceStation/CircularCylinder.js
+++ docs/TP2/implementacion/js/models/SpaceStation/CircularCylinder.js
@@ -4,16 +4,18 @@ class CircularCylinder {
         this.color = [1.0, 0.0, 0.0];
         // The curve in this case is the shape to draw multiple times.
         // Use odd numbers here.
-        this.n_curves = 43.0;
+        this.n_curves = 23.0;
         this.radius = 0.3;
         this.n_points_per_curve = 10.0;
         this.distance_from_zero = 5.0;
 
         this.pos_buf = [];
         this.nrm_buf = [];
+        this.uv_buf = [];
 
         this.base_shape_pos = null;
         this.base_shape_nrm = null;
+        this.base_shape_uv = [];
 
         this.utils = new Utils();
     }
@@ -27,20 +29,35 @@ class CircularCylinder {
             transformMatrix = mat4.create();
         }
 
+        var is_pair = true;
         for (var angle = 0.0; angle <= 2.0 * Math.PI; angle += 2.0 * Math.PI / this.n_curves) {
             var bufs = this.getShapePosBuf(angle);
             var pos = bufs[0];
             var nrm = bufs[1];
+            var uv = bufs[2];
             for (var elem of pos) {
                 this.pos_buf.push(elem);
             }
             for (var elem of nrm) {
                 this.nrm_buf.push(elem);
             }
+
+            if (is_pair) {
+                for (var elem of uv) {
+                    this.uv_buf.push(elem);
+                    this.uv_buf.push(0.0);
+                }
+                is_pair = false;
+            } else {
+                for (var elem of uv) {
+                    this.uv_buf.push(elem);
+                    this.uv_buf.push(1.0);
+                }   
+                is_pair = true;
+            }
         }
 
-
-        var grid = new Grid(this.shader, this.pos_buf, this.nrm_buf, this.color, /*n_rows=*/this.n_curves + 1.0, /*n_cols=*/this.n_points_per_curve + 1.0);
+        var grid = new Grid(this.shader, this.pos_buf, this.nrm_buf, this.color, /*n_rows=*/this.n_curves + 1.0, /*n_cols=*/this.n_points_per_curve + 1.0, this.uv_buf);
         grid.setTexture(this.texture);
         grid.draw(transformMatrix);
     }
@@ -52,6 +69,7 @@ class CircularCylinder {
                 this.base_shape_pos.push(this.distance_from_zero + this.radius * Math.cos(i * 2.0 * Math.PI / this.n_points_per_curve));
                 this.base_shape_pos.push(this.radius * Math.sin(i * 2.0 * Math.PI / this.n_points_per_curve));
                 this.base_shape_pos.push(0.0);
+                this.base_shape_uv.push(i / this.n_points_per_curve);
             }
             // We need to connect the shape's end and beginning.
             // Because of this we iterate in the other functions until n_points_per_curve + 1.
@@ -77,6 +95,6 @@ class CircularCylinder {
         mat4.fromRotation(transformation, angleToRotate, [0.0, 1.0, 0.0]);
         var pos_buf = this.utils.TransformPosBuffer(transformation, this.base_shape_pos);
         var nrm_buf = this.utils.TransformPosBuffer(transformation, this.base_shape_nrm);
-        return [pos_buf, nrm_buf];
+        return [pos_buf, nrm_buf, this.base_shape_uv];
     }
 }
